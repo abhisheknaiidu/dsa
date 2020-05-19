f701efe5dc4842eb88550540239ceabe9e031850
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-19 17:46:53 +0530

    bishu and soldiers

---
+++ Hackerearth/binary search/bishu-and-soldiers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int N,Q,P,sum=0,count=0;
+    cin >> N;
+    int a[N];
+    for(int i=0; i<N; i++) {
+        cin >> a[i];
+    }
+
+    cin >> Q;
+    for(int i=1; i<=Q; i++) {
+        cin >> P;
+        for(int j=0; j<N; j++) {
+            if(a[j]<=P) {
+                count++;
+                sum+=a[j];
+            }
+        }
+        cout << count << " " << sum << endl;
+        sum = 0;
+        count = 0;
+    }
+}
