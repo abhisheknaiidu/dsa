08b868598fc15e9d41cdd16a97bd4d2685b82f49
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-19 08:52:04 +0530

    added another method

--- Binary Search/search-on-rotated.cpp
+++ Binary Search/search-on-rotated.cpp
@@ -1,3 +1,50 @@
+// FIRST METHOD 
+
+#include <iostream>
+using namespace std;
+
+int searchElement(int a[], int n, int key) {
+    int start = 0, end = n-1;
+    int mid;
+    while(start <= end) {
+        mid = start + (end - start) / 2;
+        
+        if( a[mid] == key) return mid;
+        
+        //Element is present in first line
+        else if ( a[start] <= a[mid] ) {
+            if(key>= a[start] && key <= a[mid]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        // Elemeent in present in second line
+        else {
+            if(key >= a[mid] && key <= a[end]) {
+                start = mid + 1;
+            }
+            else end = mid -1;
+        }
+
+
+    }
+    return mid;
+}
+
+int main() {
+    
+    int a[] = { 5,6,7,1,2,3,4 } ;
+    int n = sizeof(a)/sizeof(int);
+    int key = 3;
+    cout<< searchElement(a,n,key) << endl;
+}
+
+
+
+// SECOND METHOD (FAMOUS), RECURSION!
+
 #include <iostream>
 using namespace std;
 
