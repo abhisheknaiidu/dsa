8f5b3a35a287023857b0be9f8e83617f32e098aa
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-20 22:51:12 +0530

    Agressive Cows :rocket:

--- Hackerearth/binary search/bishu-and-soldiers.cpp
+++ Hackerearth/binary search/bishu-and-soldiers.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 
 int main() {
-    int N,Q,P,sum=0,count=0;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int N, Q, P, sum = 0, count = 0;
     cin >> N;
     int a[N];
-    for(int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
         cin >> a[i];
     }
 
     cin >> Q;
-    for(int i=1; i<=Q; i++) {
+    for (int i = 1; i <= Q; i++) {
         cin >> P;
-        for(int j=0; j<N; j++) {
-            if(a[j]<=P) {
+        for (int j = 0; j < N; j++) {
+            if (a[j] <= P) {
                 count++;
-                sum+=a[j];
+                sum += a[j];
             }
         }
         cout << count << " " << sum << endl;
---
+++ SPOJ/aggresive-cows.cpp
@@ -0,0 +1,87 @@
+
+/******************************************
+* AUTHOR : Abhishek Naidu *
+* NICK : abhisheknaiidu *
+******************************************/
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define all(n)          n.begin(),n.end()
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define f               first
+#define s               second
+#define int             long long int
+#define pb              push_back
+#define mp              make_pair
+#define inf             1e18
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+void abhisheknaiidu()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main()
+{
+	abhisheknaiidu();
+
+	w(x) {
+		int N, C;
+		cin >> N >> C;
+		vector <int> a(N);
+
+		for (int i = 0; i < N; i++) {
+			cin >> a[i];
+		}
+		sort(all(a));
+
+		int s = 0, e = a[N - 1], mid, res;
+
+		while (s <= e) {
+			mid = s + (e - s) / 2 ;
+
+			int initial = 0, count = 1;
+			// cout << "mid " << mid << endl;
+			for (int i = 1; i < N && count < C ; i++) {
+				if (a[i] - a[initial] >= mid)
+					initial = i, count++;
+			}
+
+			// cout << "initial " << initial << endl;
+			// cout << "count " << count << endl;
+			if (count >= C) {
+				res = mid;
+				s = mid + 1;
+			}
+
+			else {
+				e = mid - 1;
+			}
+		}
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+
