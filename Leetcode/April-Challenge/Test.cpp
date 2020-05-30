564e505d9d4e14dd1ed860053a89429982f4fb89
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-30 21:11:05 +0530

    April 6th

---
+++ Leetcode/April-Challenge/Test.cpp
@@ -0,0 +1,65 @@
+
+/******************************************
+* AUTHOR : Abhishek Naidu *
+* NICK : abhisheknaiidu *
+******************************************/
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define all(n)          n.begin(),n.end()
+#define REP(i,a,n)      for (int i=a; i<=n; i++)
+#define N               101
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define f               first
+#define s               second
+#define int             long long int
+#define pb              push_back
+#define mp              make_pair
+#define inf             1e18
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+void abhisheknaiidu()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main()
+{
+	abhisheknaiidu();
+
+	// unordered_map <string, vector<string>> m;
+	// m.insert(pair<string, vector<string>> ({"aet"}, {"eat", "ate", "aet"}));
+
+	// vector<vector <string>> ans;
+	// for (auto x : m) {
+	// 	// cout << x.second << endl;
+	// 	ans.push_back(x.second);
+	// }
+	// for (int i = 0; i < ans.size(); ++i)
+	// {
+	// 	cout << ans[i] << endl;
+
+	// }
+
+}
+
---
+++ Leetcode/April-Challenge/best-time-to-buy-and-sell.cpp
@@ -0,0 +1,40 @@
+// Approach 1 ==>
+// instead of looking for every peak following a valley, we can simply go on crawling over the slope and keep on adding the // profit obtained from every consecutive transaction. In the end,we will be using the peaks and valleys effectively, but we // need not track the costs corresponding to the peaks and valleys along with the maximum profit, but we can directly keep // on adding the difference between the consecutive numbers of the array if the second number is larger than the first one, // and at the total sum we obtain will be the maximum profit.
+
+// Time complexity : O(n). Single pass.
+// Space complexity: O(1). Constant space needed.
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int max_profit = 0;
+		for (int i = 1; i < prices.size(); i++) {
+			if (prices[i] > prices[i - 1])
+				max_profit += prices[i] - prices[i - 1];
+		}
+		return max_profit;
+	}
+};
+
+// Approach 2 ==> Peak-Valley Approach
+// TotalProfit=∑i(height(peaki)−height(valleyi))
+// Peaks and Valleys should be calculated only taking
+// next element as a reference always!
+vi v;
+v = {7, 1, 5, 3, 6, 4};
+int i = 0;
+int peak = v[0];
+int valley = v[0];
+int max_profit = 0;
+
+// cout << peak << valley ;
+while (i < v.size() - 1) {
+	while (i < v.size() - 1 && v[i] >= v[i + 1])
+		i++;
+	valley = v[i];
+	while (i < v.size() - 1 && v[i] <= v[i + 1])
+		i++;
+	peak = v[i];
+	max_profit += peak - valley;
+}
+cout << max_profit;
