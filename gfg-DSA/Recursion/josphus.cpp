b82846b2eeaad321c3f3a43c724164c3fa696d7f
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-08 19:38:55 +0530

    Changes

--- gfg-DSA/Recursion/josphus.cpp
+++ gfg-DSA/Recursion/josphus.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 int Jos(int n, int k) {
-// for handling 0-n
-// Adding  because, the next position being shifted by k position.
+// for handling 0-n (here the recursive call gives)
+// Adding k because, the next position being shifted by k position.
+
 // return n > 1 ? (Jos(n-1, k) + k ) % n  : 1;
-// for handling 0-n-1, we needs this only
+// here the recursive call gives 0-n-1, 
+// Adding 1 in RHS part, to ensure that we are in the same 
+// range of 1-n, and also we are having 1 extra player in
+// previous case.
 return n > 1 ? (josephus(n-1, k) + k - 1) % n + 1 : 1;
 }
 
