de6f1aa4229025f97eb3ad9ef4944162bce67f80
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-09 18:40:47 +0530

    Arrays

---
+++ gfg-DSA/Arrays/Basics.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Reverse an array
+    // int arr[] = { 3,5,6,7 };
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    // reverse(arr, arr+n);
+    // for(int i=0; i<n; i++) {
+    //     cout << arr[i] << endl;
+    // }
+    return 0;
+}
