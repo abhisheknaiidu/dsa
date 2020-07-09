842b706b96e8f412ef034357b4b3860444bd2076
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-09 19:46:07 +0530

    Rotate an array

--- gfg-DSA/Arrays/Basics.cpp
+++ gfg-DSA/Arrays/Basics.cpp
@@ -9,5 +9,16 @@ int main() {
     // for(int i=0; i<n; i++) {
     //     cout << arr[i] << endl;
     // }
+    // Rotate the array through a given a 'd'
+    int arr[] = { 3,4,5,6 };
+    int d=2;
+    int n= sizeof(arr) / sizeof(arr[0]);
+    // reverse from d=2 => 5,6,4,3
+    reverse(arr[],0,d-1); // 4,3,5,6
+    reverse(arr[],d,n-1); // 4,3,6,5
+    reverse(arr[],0,n-1); // 5,6,3,4
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << endl;
+    }
     return 0;
 }
