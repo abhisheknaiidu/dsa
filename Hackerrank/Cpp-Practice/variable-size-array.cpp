b978580d337efd21dcfafca551174e20c80f6b5b
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-22 23:46:52 +0530

    Hackrank Practice

--- Hackerrank/Cpp-Practice/variable-size-array.cpp
+++ Hackerrank/Cpp-Practice/variable-size-array.cpp
@@ -45,15 +45,15 @@
 #define setbits(x)      __builtin_popcountll(x)
 #define zrobits(x)      __builtin_ctzll(x)
 #define mod             1000000007
-#define f               first
+#define fi               first
 #define s               second
 #define pb              push_back
 #define mp              make_pair
 #define lli             long long int
 #define inf             1e18
 #define w(x)            int x; cin>>x; while(x--)
-#define f(i,a,b) for(i=a;i<b;i++)
-#define fd(i,a,b) for(i=a;i>=b;i--)
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(int i=a;i>=b;i--)
 #define out cout<<
 #define in cin>>
 using namespace std;
@@ -70,17 +70,27 @@ void abhisheknaiidu()
 
 int main(int argc, char* argv[]) {
 	abhisheknaiidu();
+	int n,q,size,values,which,index;
+    cin >> n >> q;
+    vector < vi > v;
 
-w(x) {
-	lli ts;
-	in ts;
 
-    out 
-}
+    for(int i=0; i<n; i++) {
+    	cin >> size;
+    	vi vnew;
+    	for(int j=0; j<size; j++) {
+    		cin >> values;
+    		vnew.pb(values);
+    	} 
+    	v.pb(vnew);
+    }
 
+    for(int k=0; k<q; k++) {
+    	cin >> which >> index;
+    	cout << v[which][index] << endl;
+    }
    return 0;
 }
-
 
 
 
