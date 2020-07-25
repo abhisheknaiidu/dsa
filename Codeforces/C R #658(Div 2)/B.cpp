5e0478c9fbb78ed8fb1fab756c862fefcc75f6f3
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-25 06:02:50 +0530

    B problem

---
+++ Codeforces/C R #658(Div 2)/B.cpp
@@ -0,0 +1,127 @@
+/******************************************
+* AUTHOR : Abhishek Naidu *
+* NICK : abhisheknaiidu *
+******************************************/
+#pragma GCC optimize("Ofast")
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <cstdlib> 
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <tuple>
+#include <string>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define ll long long 
+#define all(n)          n.begin(),n.end()
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define w(x)            int x; cin>>x; while(x--)
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define fi               first
+#define s               second
+#define pb              push_back
+#define mp              make_pair
+#define lli             long long int
+#define inf             1e18
+#define w(x)            int x; cin>>x; while(x--)
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(int i=a;i>=b;i--)
+#define out cout<<
+#define in cin>>
+using namespace std;
+
+void abhisheknaiidu()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+int main(int argc, char* argv[]) {
+	abhisheknaiidu();
+
+w(x) {
+	int n,a;
+	cin >> n;
+
+	vi v;
+	f(i,0,n) {
+		cin >> a;
+		v.pb(a);
+	}
+
+	bool ans = 1;
+
+	f(i,0,n) {
+		if(v[i] == 1) {
+			ans = !ans;
+		}
+		else {
+			ans = !ans;
+			break;
+		}
+	}
+
+	if(!ans) cout << "First" << endl;
+
+	else cout << "Second" << endl;
+
+	// f(i,0,n) {
+	// 	cout << v[i] << " ";
+	// }
+	// cout << endl;
+}
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
