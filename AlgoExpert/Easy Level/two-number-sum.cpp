a6b08686241c883c9eacb88153a5926e82e3f9d6
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-31 16:24:33 +0530

    2-numbers

--- AlgoExpert/Easy Level/two-number-sum.cpp
+++ AlgoExpert/Easy Level/two-number-sum.cpp
@@ -1,105 +1,18 @@
-/******************************************
-* AUTHOR : Abhishek Naidu *
-* NICK : abhisheknaiidu *
-******************************************/
-#pragma GCC optimize("Ofast")
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <cstdlib> 
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <tuple>
-#include <string>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#define min(a,b) ((a)<(b)?(a):(b))
-#define max(a,b) ((a)>(b)?(a):(b))
-#define ll long long 
-#define all(n)          n.begin(),n.end()
-#define pii             pair<int,int>
-#define vi              vector<int>
-#define mii             map<int,int>
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define w(x)            int x; cin>>x; while(x--)
-#define pqb             priority_queue<int>
-#define pqs             priority_queue<int,vi,greater<int> >
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-#define mod             1000000007
-#define fi               first
-#define s               second
-#define pb              push_back
-#define mp              make_pair
-#define lli             long long int
-#define inf             1e18
-#define w(x)            int x; cin>>x; while(x--)
-#define f(i,a,b) for(int i=a;i<b;i++)
-#define fd(i,a,b) for(int i=a;i>=b;i--)
-#define out cout<<
-#define in cin>>
-using namespace std;
-
-void abhisheknaiidu()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-}
-
-	vi twoNumberSum(vi array, int targetSum) {
-		f(i,0,array.size()) {
-			cout << array[i];
-		}
-		return {};
-	}
-
-// int main(int argc, char* argv[]) {
-// 	abhisheknaiidu();
-
-// 	twoNumberSum();
-
-
-
-//    return 0;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        vector <int> ans;
+        for(int i=0; i<nums.size(); i++) {
+            int want = target - nums[i];
+            if(hash.find(want) != hash.end()) {
+                ans.push_back(hash[want]);
+                ans.push_back(i);
+                return ans;
+            }else {
+                hash[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
