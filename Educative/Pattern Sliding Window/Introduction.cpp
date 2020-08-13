fc92116abf2a5200a466e59a26ea6b4d1047c4bc
abhisheknaiidu <geekyabhi12@gmail.com>
2020-08-13 20:46:06 +0530

    Pattern Sliding Window

--- Educative/Pattern Sliding Window/Introduction.cpp
+++ Educative/Pattern Sliding Window/Introduction.cpp
@@ -1,3 +1,6 @@
+//  Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+
+
 #pragma GCC optimize("Ofast")
 #include <algorithm>
 #include <bitset>
@@ -32,6 +35,31 @@ void abhisheknaiidu()
 
 int main(int argc, char* argv[]) {
 	abhisheknaiidu();
+
+
+	vector <int> v{1, 3, 2, 6, -1, 4, 1, 8, 2};
+    int n = v.size();
+    int k=5;
+
+
+	vector <double> res(n-k+1);
+
+	int i=0;
+	double sum=0;
+	for(int j=0; j<n; j++) {
+		sum+=v[j];
+		if(j>=k-1) {
+			res[i] = (sum/5);
+			sum = sum - v[i];
+			i++;
+		}
+	}
+
+	// for (int i = 0; i <res.size(); ++i)
+	// {
+	// 	cout <<  res[i] << endl;
+	// }
+
 
 
    return 0;
