e81d6170402cd840b27632be9d6f430bce037466
abhisheknaiidu <geekyabhi12@gmail.com>
2020-08-13 07:26:17 +0530

    Rotate Array 2

--- Leetcode/Problems/Rotate Array.cpp
+++ Leetcode/Problems/Rotate Array.cpp
@@ -12,3 +12,27 @@ public:
         }
     }
 };
+
+// OR
+
+
+class Solution {
+public:
+    void reverse(vector <int> &nums, int s, int e) {
+        while(s<e) {
+            int t = nums[s];
+            nums[s] = nums[e];
+            nums[e] = t;
+            s++;
+            e--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+};
+
