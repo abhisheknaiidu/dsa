88b432b9409192d33bd7f5c4f1a2986f9d8b6fb3
abhisheknaiidu <geekyabhi12@gmail.com>
2020-08-16 16:36:12 +0530

    Optimised method

--- Leetcode/Problems/Medium/remove-nth-node-from-end.cpp
+++ Leetcode/Problems/Medium/remove-nth-node-from-end.cpp
@@ -11,32 +11,50 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-    ListNode* d = new ListNode(0);
-      d->next = head;
-      int count=0;
-      ListNode *temp = head;
-      while(temp != NULL) {
-          count++;
-          temp = temp->next;
+      int len = 1;
+      ListNode* fast = head;
+      ListNode* slow = head;
+      while (fast->next) {
+          len++;
+          fast = fast->next;
+          if(len > n+1) {
+              slow = slow->next;
+          }
       }
-      // cout << count;
-      if(count == 1) {
-          // ListNode* td = head;
-          head = NULL;
-          return head;
-      }
-      cout << count << endl;
-      count = count - n;
-      // temp = d;
-      ListNode* temp2 = d;
-      while(count>0) {
-          cout<< count;
-          count--;
-          temp2 = temp2->next;
-      }
-      ListNode* temp3 = temp2->next;
-      temp2->next = temp3->next;
-      temp3->next = NULL;
-      return d->next;
+    if(len == n) {
+        return head->next;
+    }
+    else {
+        slow->next = slow->next->next;
+    }
+    return head;
+    // ListNode* d = new ListNode(0);
+    //   d->next = head;
+    //   int count=0;
+    //   ListNode *temp = head;
+    //   while(temp != NULL) {
+    //       count++;
+    //       temp = temp->next;
+    //   }
+    //   // cout << count;
+    //   if(count == 1) {
+    //       // ListNode* td = head;
+    //       head = NULL;
+    //       return head;
+    //   }
+    //   cout << count << endl;
+    //   count = count - n;
+    //   // temp = d;
+    //   ListNode* temp2 = d;
+    //   while(count>0) {
+    //       cout<< count;
+    //       count--;
+    //       temp2 = temp2->next;
+    //   }
+    //   ListNode* temp3 = temp2->next;
+    //   temp2->next = temp3->next;
+    //   temp3->next = NULL;
+    //   return d->next;
+
     }
 };
