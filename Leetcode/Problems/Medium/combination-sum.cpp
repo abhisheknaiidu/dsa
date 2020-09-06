9094c12cba9380ad5844e6fe83d5c6e7db1fe550
abhisheknaiidu <geekyabhi12@gmail.com>
2020-09-06 06:01:22 +0530

    combination-sum

---
+++ Leetcode/Problems/Medium/combination-sum.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast")
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+void abhisheknaiidu()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve(vector<int> candidates, vector<int> sub, vector<vector<int>>& res, int target, int start ) {
+
+	if(target == 0)
+	res.push_back(sub);
+    if(target < 0) return;
+	for(int i=start; i<candidates.size(); i++) {
+		// cout << "start " << start << " " << "i " << i << " " << "target " << target << endl;
+		sub.push_back(candidates[i]);
+		solve(candidates, sub, res, target-candidates[i], i);
+
+		sub.pop_back();
+	}
+}
+
+
+int main(int argc, char* argv[]) {
+	abhisheknaiidu();
+
+	vector<int> candidates{2,3,5};
+	int target = 8;
+	vector<vector<int>> res;
+	vector<int> sub;
+	solve(candidates, sub, res, target, 0);
+
+	for(auto x: res) {
+		for(auto s: x) {
+			cout << s <<  " ";
+		}
+		cout << endl;
+	}
+
+   return 0;
+}
