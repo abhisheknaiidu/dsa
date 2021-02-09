652515a856eb5afb306fb57b1e386e4dcc972768
abhisheknaiidu <geekyabhi12@gmail.com>
2021-02-09 18:05:25 +0530

    Longest Sum

---
+++ DSA/Trees/longest-sum.cpp
@@ -0,0 +1,63 @@
+#pragma GCC optimize("Ofast")
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+void abhisheknaiidu()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+};
+
+// utility fxn
+Node* newNode(int n) {
+	Node* temp = new Node;
+	temp->data = n;
+	temp->left = temp->right = NULL;
+	return(temp);
+}
+
+int maxSum(Node* root) {
+	if(root == NULL) return 0;
+	int left = maxSum(root->left);
+	int right = maxSum(root->right);
+	return max(left,right) + root->data;
+}
+
+int main(int argc, char* argv[]) {
+	abhisheknaiidu();
+
+	Node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+	// int sum = 0;
+	cout << maxSum(root) << endl;
+
+
+   return 0;
+}
