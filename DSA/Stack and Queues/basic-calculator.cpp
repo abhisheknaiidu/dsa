eea17007519edf4651f9caa1cb035cf565c996b9
abhisheknaiidu <geekyabhi12@gmail.com>
2021-02-16 18:14:36 +0530

    basic-calculator

--- DSA/Stack and Queues/basic-calculator.cpp
+++ DSA/Stack and Queues/basic-calculator.cpp
@@ -27,7 +27,40 @@ void abhisheknaiidu()
 
 int main(int argc, char* argv[]) {
 	abhisheknaiidu();
-	
+
+	string s = "22-3*5";
+	int currentNumber = 0;
+	int n = s.size();
+	stack<int> st;
+	char operation = '+';
+	for(int i=0; i<n; i++) {
+		char currentChar = s[i];
+		if(isdigit(currentChar)) {
+			currentNumber = (currentNumber*10) + (currentChar - '0');
+		}
+		if(!isdigit(currentChar) && !iswspace(currentChar) || i==n-1) {
+			if(operation == '-') st.push(-currentNumber);
+			else if(operation == '+') st.push(currentNumber);
+			else if(operation == '*') {
+				int stackTop = st.top();
+				st.pop();
+				st.push(stackTop*currentNumber);
+			}
+			else if(operation == '/') {
+				int stackTop = st.top();
+				st.pop();
+				st.push(stackTop/currentNumber);
+			}
+			operation = currentChar;
+			currentNumber = 0;
+		}	
+	}
+	int res = 0;
+	while(!st.empty()) {
+		res += st.top();
+		st.pop();
+	}
+	cout << res << endl;
 
    return 0;
 }
